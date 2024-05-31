5dfbf498a9589b93658404747edc6fafabfa5631
GabrielaBillino <gabrielabillino@gmail.com>
2024-05-31 00:11:36 -0300

    faltaba un cambio guardar

--- ObligatorioDDA/src/controlador/TableroController.java
+++ ObligatorioDDA/src/controlador/TableroController.java
@@ -1,7 +1,30 @@
 
 package controlador;
 
+import dominio.Parking;
+import java.util.ArrayList;
+import java.util.List;
+import vista.VistaTablero;
+
 
 public class TableroController {
+    private List<Parking> parkings = new ArrayList<>();
+    private VistaTablero vista;
+
+    public TableroController(List<Parking> parkings, VistaTablero vista) {
+        this.vista = vista;
+        this.parkings = parkings;
+        mostrarTitulo();
+        cargarTabla();
+    }
+    
+    
+    public void mostrarTitulo(){
+        vista.mostrarTitulo("Tablero de control");
+    }
+    
+    public void cargarTabla(){
+        vista.cargarTabla(parkings);
+    }
     
 }
